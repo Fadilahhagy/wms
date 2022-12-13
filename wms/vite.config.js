30a0e826a289fe677bd8799d433ba2d8ccd851f0
Fadilahhagy <hagifadillah@gmail.com>
2022-12-13 11:27:42 +0700

    bismillah

--- wms/vite.config.js
+++ wms/vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/assets/modules/nicescroll/jquery.nicescroll.min.js',
+                    'resources/assets/modules/jquery.min.js',
+                    'resources/assets/modules/bootstrap/css/bootstrap.min.css', 
+                    'resources/assets/modules/fontawesome/css/all.min.css',
+                    'resources/assets/css/components.css',
+                    'resources/assets/css/style.css',
+                    'resources/assets/modules/popper.js',
+                    'resources/assets/modules/tooltip.js',
+                    'resources/assets/modules/bootstrap/js/bootstrap.min.js',
+                    'resources/assets/modules/moment.min.js',
+                    'resources/assets/js/stisla.js',
+                    'resources/assets/js/scripts.js',
+                    'resources/assets/js/custom.js',
+            ],
             refresh: true,
         }),
     ],
